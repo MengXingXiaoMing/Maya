476e626d0287ca91c5e1dbe1a8995845c401c81c
MengXingXiaoMing <128672103+MengXingXiaoMing@users.noreply.github.com>
2024-03-04 23:16:43 +0800

    Delete ZhanKangMing directory

--- ZhanKangMing/Maya/MayaUI/SliderEdit.py
+++
@@ -1,9 +0,0 @@
-#coding=gbk
-import maya.cmds as cmds
-import pymel.core as pm
-#加载文本
-class ZKM_SliderClass:
-    # 滑条滑动
-    def ZKM_Slider(self,SliderLine, Window, From, Num):
-        num = pm.floatScrollBar(SliderLine, q=1, v=1)
-        pm.formLayout(From, edit=1, attachPosition=(Window, "top", (num * Num), 0), attachForm=(Window, "left", 0))
